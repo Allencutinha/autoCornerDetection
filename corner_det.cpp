c11cb36c0822dd5870bc81632d23fd76b170bf39
Allen Frank Cutinha <allencutinha@gmail.com>
2022-05-22 15:25:03 +0200

    corner det added

---
+++ corner_det.cpp
@@ -0,0 +1,9 @@
+#include"corner_det.h"
+
+int findChessboardCorners3( const cv::Mat& arr, CvSize pattern_size,
+                             CvPoint2D32f* out_corners, int* out_corner_count,
+                             int min_number_of_corners )
+{
+cv::imshow("corner_det", arr);
+cv::waitKey(0);
+}
